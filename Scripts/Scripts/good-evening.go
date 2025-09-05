ebde6a8e18209acfa92c770adb3e2fd98b08a6b8
Justin Sniesak <justindsniesak@gmail.com>
2025-09-04 19:34:49 -0700

    Greeting and name scripts

---
+++ Scripts/Scripts/good-evening.go
@@ -0,0 +1,8 @@
+// This defines the package to use
+package define
+// This imports the fmt package 
+import "fmt"
+// This calls the main function, and then prints "Good Evening" to the terminal
+func main() {
+	fmt.Println("Good Evening")
+}
