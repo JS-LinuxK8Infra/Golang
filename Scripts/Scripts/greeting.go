44a301762e2ce86f22ef773cf8b56ef3be432c39
Justin Sniesak <justindsniesak@gmail.com>
2025-09-04 19:42:35 -0700

    Add files via upload

---
+++ Scripts/Scripts/greeting.go
@@ -0,0 +1,12 @@
+package desktop
+// This defines the package being called
+package main
+
+// This imports the format package
+import ("fmt")
+
+// This the main function, defines the string to be output to the terminal, and prints it
+func main() {
+	greeting := "Hi, my name is Justin"
+	fmt.Println(greeting)
+}
