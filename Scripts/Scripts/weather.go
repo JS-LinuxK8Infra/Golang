b604b5d01e73c9dff38edcb65dee440d9072afa3
Justin Sniesak <justindsniesak@gmail.com>
2025-09-06 19:32:12 -0700

    Weather script

---
+++ Scripts/Scripts/weather.go
@@ -0,0 +1,11 @@
+package desktop
+// This defines the package to use
+package main
+
+// This imports the fmt package
+import "fmt"
+
+// This calls the main function, and then prints "The weather is nice today" to the terminal
+func main() {
+	fmt.Println("The weather is nice today")
+}
