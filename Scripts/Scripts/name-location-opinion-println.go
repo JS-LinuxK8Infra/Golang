2f0b599e64621483b5e0f7a28e104c9661d469d9
Justin Sniesak <justindsniesak@gmail.com>
2025-09-06 21:46:58 -0700

    print variables using Println function

---
+++ Scripts/Scripts/name-location-opinion-println.go
@@ -0,0 +1,18 @@
+package desktop
+// This defines the package to use
+package main
+
+// This imports the fmt package
+import "fmt"
+
+// This calls the main function, assigns a string to two different variables, then joins a string to both variables and prints each to a newline on the terminal
+func main() {
+
+	var name string = "My name is Justin, "
+	var location string = "I live in Seattle, "
+	var opinion string = "and I love it here."
+
+	fmt.Println(name)
+	fmt.Println(location)
+	fmt.Print(opinion)
+}
