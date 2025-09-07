923d0de3121c053b540f20ff56de1ff977207d88
Justin Sniesak <justindsniesak@gmail.com>
2025-09-07 10:55:53 -0700

    Add files via upload

---
+++ Scripts/Scripts/shorthand.go
@@ -0,0 +1,16 @@
+// Define the package to use
+package main
+
+// Import the fmt package
+import "fmt"
+
+// Call the main function, define two variables using two differant data types both using shorthand and outputting each to the terminal on a seperate line
+func main() {
+
+	name := "Justin"
+	moved := "06/2025"
+
+	fmt.Println(name)
+	fmt.Println(moved)
+
+}
