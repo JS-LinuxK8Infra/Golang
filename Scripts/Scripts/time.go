d56e082b8dd6d1801f17bff062766d77c740ad7c
Justin Sniesak <justindsniesak@gmail.com>
2025-09-06 20:06:03 -0700

    time variable script

---
+++ Scripts/Scripts/time.go
@@ -0,0 +1,11 @@
+// This defines the package to use
+package main
+
+// This imports the fmt package
+import "fmt"
+
+// This assigns the string to the time variable and then prints the output to the terminal when it is called
+func main() {
+	var time string = "It is Saturday, September 6th when I wrote this"
+	fmt.Println(time)
+}
