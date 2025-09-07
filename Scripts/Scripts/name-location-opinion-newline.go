8cce28b220503077d079a58d3c94fba4b248f388
Justin Sniesak <justindsniesak@gmail.com>
2025-09-06 20:56:46 -0700

    Add VAR and \n scripts

---
+++ Scripts/Scripts/name-location-opinion-newline.go
@@ -0,0 +1,18 @@
+package desktop
+// This defines the package to use
+package main
+
+// This imports the fmt package
+import "fmt"
+
+// This calls the main function, assigns a string to two different variables, then joins a string to both variables and prints to the terminal
+func main() {
+
+	var name string = "My name is Justin, "
+	var location string = "I live in Seattle, "
+	var openion string = "and I love it here."
+
+	fmt.Print(name, "\n")
+	fmt.Print(location, "\n")
+	fmt.Print(openion)
+}
