203759f599ff63c57c8577c402f3136f404d8c78
Justin Sniesak <justindsniesak@gmail.com>
2025-09-10 21:55:36 -0700

    Expanded shorthand script

---
+++ Scripts/Scripts/shorthand_expanded.go
@@ -0,0 +1,19 @@
+package desktop
+// Define the package to use
+package main
+
+// Import the fmt package
+import "fmt"
+
+// Call the main function, define two string variables and one integer variable using shorthand and outputting each to the terminal on a seperate line
+func main() {
+
+	name := "Justin"
+	moved := "June"
+	year := 2025
+
+	fmt.Println(name)
+	fmt.Println(moved)
+	fmt.Println(year)
+
+}
