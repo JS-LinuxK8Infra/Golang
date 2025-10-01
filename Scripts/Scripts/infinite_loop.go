29b56411e17fa26a7cc4956828d7a7a7703ffad8
Justin Sniesak <justindsniesak@gmail.com>
2025-09-30 22:22:28 -0700

    Infinite Loop

---
+++ Scripts/Scripts/infinite_loop.go
@@ -0,0 +1,16 @@
+// Define the package to use
+package main
+
+// Import the fmt package
+import "fmt"
+
+func main() {
+
+	//Infinite loop that increments the jt var increasing by 1 each time and will not terminate until manually ended
+	jt := 14
+	for {
+		jt++
+	}
+	fmt.Println(jt)
+
+}
