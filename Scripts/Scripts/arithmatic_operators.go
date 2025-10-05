43ca8eeb313efa081cef51f2ed126ebf9a91baa3
Justin Sniesak <justindsniesak@gmail.com>
2025-10-05 16:51:17 -0700

    Refactor arithmatic_operators.go to best practices

--- Scripts/Scripts/arithmatic_operators.go
+++ Scripts/Scripts/arithmatic_operators.go
@@ -1,38 +1,38 @@
-// This defines the package being called
 package main
 
-// This imports the format package
 import (
 	"fmt"
 )
 
-// Call the main function
-func main() {
-	//Define two string variables
-	var s, w string = "Seattle, ", "Washington"
-	//Define two float64 variables
-	var j, t float64 = 18.18, 1.1
-	//Defines two integers
-	var r, l int = 7, 2
-	//Defines an int and increments it by one
-	var a int = 18
-	a++
-	//Defines an int and decrements it by one
-	var b int = 20
-	b--
-	//Adds both string variables together and combines them
-	fmt.Println(s + w)
-	//Prints the second decimal place, subtacts the second float64 var from the first float64 var
-	fmt.Println(j - t)
-	//Multiplies the left and right operand
-	fmt.Println(r * l)
-	//Divides the left operand by the right operand
-	fmt.Println(r / l)
-	//Returns the remainder after the left operand is divided by the right operand
-	fmt.Println(r % l)
-	//Returns the var and increments it by 1
-	fmt.Println(a)
-	//Returns the var and decrements it by 1
-	fmt.Println(b)
+func stringAddition() {
+	city, state := "Seattle, ", "Washington"
+	fmt.Println(city + state)
+}
 
+func float64Arithmetic() {
+	float1, float2 := 18.18, 1.1
+	fmt.Println(float1 - float2)
+}
+
+func intArithmetic() {
+	int1, int2 := 7, 2
+	fmt.Println(int1 * int2)
+	fmt.Println(int1 / int2)
+	fmt.Println(int1 % int2)
+}
+
+func intIncrementDecrement() {
+	num1 := 18
+	num1++
+	num2 := 20
+	num2--
+	fmt.Println(num1)
+	fmt.Println(num2)
+}
+
+func main() {
+	stringAddition()
+	float64Arithmetic()
+	intArithmetic()
+	intIncrementDecrement()
 }
