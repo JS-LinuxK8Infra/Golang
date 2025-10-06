b1b778ff95f036496f98bb933c63625f00843161
Justin Sniesak <justindsniesak@gmail.com>
2025-10-05 17:43:59 -0700

    Update float64ToInt.go

--- Scripts/Scripts/float64ToInt.go
+++ Scripts/Scripts/float64ToInt.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-// Call the main function, assigns a string and a float to two different variables, then uses Printf to combine each in a string to the terminal
 func floatToInt() {
 	f64 := 96.96
 	int1 := int(f64)
