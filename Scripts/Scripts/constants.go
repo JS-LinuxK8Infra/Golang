9b066b89c1e02a0eefde4d10262b84f3095a5d29
Justin Sniesak <justindsniesak@gmail.com>
2025-10-05 17:25:09 -0700

    Refactor constants.go to best practices

--- Scripts/Scripts/constants.go
+++ Scripts/Scripts/constants.go
@@ -1,17 +1,17 @@
-// This defines the package being called
 package main
 
-// This imports the format package
 import (
 	"fmt"
 )
 
-// This the main function, defines the string to be output to the terminal, and prints it
+func constantDivisor() {
+	decade := 10
+	leapyear := 4
+	totalLeapYear := decade / leapyear
+
+	fmt.Println("There are", totalLeapYear, "leap years in a decade.")
+}
 
 func main() {
-	const decade = 10
-	const leapyear = 4
-	const total_leap_year = decade / leapyear
-
-	fmt.Println("There are", total_leap_year, "leap years in a decade.")
+	constantDivisor()
 }
