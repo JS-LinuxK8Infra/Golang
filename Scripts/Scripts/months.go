28f86d68529fa9b5112c701687ad8be9dbd96a5a
Justin Sniesak <justindsniesak@gmail.com>
2025-10-05 18:38:31 -0700

    Refactor months.go to best practices

--- Scripts/Scripts/months.go
+++ Scripts/Scripts/months.go
@@ -1,15 +1,13 @@
-// Define the package to use
 package main
 
-// Import the fmt package
 import "fmt"
 
-// Call the main function, assigns a string and a float to two different variables, use Printf to out each in a string to the terminal
+func strFlt64Convert() {
+	name := "Justin,"
+	months := 3.5
+	fmt.Printf("%v there are %.1f months left in the year!\n", name, months)
+}
+
 func main() {
-
-	var name string = "Justin,"
-	var months float64 = 3.5
-
-	fmt.Printf("%v there are %.1f months left in the year!", name, months)
-
+	strFlt64Convert()
 }
