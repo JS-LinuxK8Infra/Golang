d2d92d3d6085bd3310817d98f93289f837a194fe
Justin Sniesak <justindsniesak@gmail.com>
2025-10-05 18:55:30 -0700

    Update and rename name-location-opinion-newline.go to nameLocationOpinion-newline.goand refactor to best practices

---
+++ Scripts/Scripts/nameLocationOpinion-newline.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func nameLocationOpinion() {
+	name := "My name is Justin, "
+	location := "I live in Seattle, "
+	opinion := "and I love it here."
+	fmt.Println(name, location, opinion)
+}
+
+func main() {
+	nameLocationOpinion()
+}
