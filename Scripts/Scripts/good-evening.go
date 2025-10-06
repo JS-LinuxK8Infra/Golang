105e6dada5fad421df7d62e00a03059fdb793c66
Justin Sniesak <justindsniesak@gmail.com>
2025-10-05 18:03:09 -0700

    Update and rename good-evening.go to goodEvening.go and refactor to best practices

--- Scripts/Scripts/good-evening.go
+++
@@ -1,8 +0,0 @@
-// This defines the package to use
-package main
-// This imports the fmt package 
-import "fmt"
-// This calls the main function, and then prints "Good Evening" to the terminal
-func main() {
-	fmt.Println("Good Evening")
-}
---
+++ Scripts/Scripts/goodEvening.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func goodEvening() {
+	fmt.Println("Good Evening!")
+}
+
+func main() {
+	goodEvening()
+}
